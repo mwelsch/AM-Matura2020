c14b1c03294e019a0bb5da78d68e827985e25c7d
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-10-10 00:24:14 +0200

    Script now waits for download to complete!

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,18 @@
 # Overleaf Backup Solution
 
 ## Requirements
-ChromeDriver: https://chromedriver.chromium.org/downloads
+* Chrome installed
+* Compatible [ChromeDriver](https://chromedriver.chromium.org/downloads) installed
+* JSON-File `test.json` with following structure:
+```json
+{
+  "email": "<your@mail>",
+  "password": "<password>",
+  "document": "<document_title>"
+}
+```
 
 ## Sources
 * [Fix your Chromedriver](https://stackoverflow.com/a/52108199)
+* [Wait for download to complete](https://stackoverflow.com/a/48267887)
+* [Turn it on and off again - fixed site loading](https://www.youtube.com/watch?v=p85xwZ_OLX0)
--- obs.py
+++ obs.py
@@ -1,48 +1,55 @@
-import time
-from time import sleep
-from selenium.webdriver.support import expected_conditions as ec
+import json
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import json
-
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
+
+def download_wait(og_driver):
+    if not og_driver.current_url.startswith("chrome://downloads"):
+        og_driver.get("chrome://downloads/")
+    return og_driver.execute_script("""
+        var items = downloads.Manager.get().items_;
+        if (items.every(e => e.state === "COMPLETE"))
+            return items.map(e => e.fileUrl || e.file_url);
+        """)
+
+
 if __name__ == "__main__":
+    # Read config
     with open('test.json') as json_file:
         data = json.load(json_file)
-
+    # Configure browser
     opp = Options()
     opp.add_argument('--blink-settings=imagesEnabled=false')
     opp.add_argument('--no-proxy-server')
     opp.add_argument('--disable-gpu')
     opp.add_argument('--hide-scrollbars')
+    # Start it!
     driver = webdriver.Chrome(options=opp)
-
-
-    driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36", "platform": "Windows"})
+    driver.execute_cdp_cmd("Network.setUserAgentOverride", {
+        "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+        "platform": "Windows"})
+    # Go to overleaf
     try:
         driver.get("https://www.overleaf.com/login")
-        #driver.get("https://youtube.com")
+        # driver.get("https://youtube.com")
     except TimeoutException:
         driver.execute_script("window.stop();")
-
+    # Log in
     driver.find_element_by_name("email").send_keys(data["email"])
     driver.find_element_by_name("password").send_keys(data["password"])
     driver.find_element_by_name("password").send_keys(Keys.ENTER)
-    #WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "/html/body/main/div[2]/div/div/div[2]/div[2]/div/div/ul/table/tbody/tr[2]/td[4]/div/button[2]"))).click()
-    # Working: //*[text() = 'Urbaniec_5AHIT_SYT_Data_Science']
-    # //tr/td/div/span/a[text() = 'Urbaniec_5AHIT_SYT_Data_Science']
-    # //tr[td/div/span/a[text() = 'Urbaniec_5AHIT_SYT_Data_Science']]/td/div/button[2]
-    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, "//tr[td/div/span/a[text() = '" + data["document"] + "']]/td/div/button[2]"))).click()
-
-    #name = driver.find_element_by_xpath("//tr[td/div/span/a[text() = '" + data["document"] + "']]/td/div/button[2]")
-    #print(str(name))
-
-    print(str(driver.current_url))
-
-
-    print("Hello world")
+    # Download wanted document
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable(
+        (By.XPATH, "//tr[td/div/span/a[text() = '" + data["document"] + "']]/td/div/button[2]"))).click()
+    # Wait for donwload to complete
+    driver.implicitly_wait(1)
+    paths = WebDriverWait(driver, 120, 1).until(download_wait)
+    driver.implicitly_wait(2)
+    # Quit it!
+    driver.quit()
