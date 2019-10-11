bb667ae73488f0c7beec071a1332139f430533ef
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-10-11 17:19:38 +0200

    Program done for now - TODO ReadMe

--- obs.py
+++ obs.py
@@ -24,27 +24,24 @@ def download_wait(og_driver):
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) >= 2:
         task = sys.argv[1]
         directory = os.getcwd()
 
         if task == "init":
+            obs_conf = ('{\n'
+                        '  "email": "<your@mail>",\n'
+                        '  "password": "<password>",\n'
+                        '  "document": "<document_title>",\n'
+                        '  "version" : 0.0\n'
+                        '}')
             with open("config.json", "w+") as config:
-                config.write("""
-                {
-                  "email": "<your@mail>",
-                  "password": "<password>",
-                  "document": "<document_title>"
-                  "version" : 0.0
-                }
-                """)
+                config.write(obs_conf)
             with open(".gitignore", "w+") as gitignore:
-                config.write("config.json")
+                gitignore.write("config.json")
             subprocess.call(["git", "init"])
             if len(sys.argv) >= 3:
                 subprocess.call(["git", "remote", "add", "origin", str(sys.argv[2])])
-                subprocess.call(["git", "branch", "-u", "origin/master"])
 
         elif task == "do":
             # Read config
@@ -56,6 +53,11 @@ if __name__ == "__main__":
             opp.add_argument("--no-proxy-server")
             opp.add_argument("--disable-gpu")
             opp.add_argument("--hide-scrollbars")
+            prefs = {"profile.default_content_settings.popups": 0,
+                     "download.default_directory": r"" + directory + "\\",
+                     # IMPORTANT - ENDING SLASH V IMPORTANT
+                     "directory_upgrade": True}
+            opp.add_experimental_option("prefs", prefs)
             opp.add_argument("download.default_directory=" + directory)
             # Start it!
             driver = webdriver.Chrome(options=opp)
@@ -73,27 +75,39 @@ if __name__ == "__main__":
             driver.find_element_by_name("password").send_keys(data["password"])
             driver.find_element_by_name("password").send_keys(Keys.ENTER)
             # Download wanted document
-            WebDriverWait(driver, 20).until(ec.element_to_be_clickable(
+            WebDriverWait(driver, 200).until(ec.element_to_be_clickable(
                 (By.XPATH, "//tr[td/div/span/a[text() = '" + data["document"] + "']]/td/div/button[2]"))).click()
+            # Logout
+            driver.get("https://www.overleaf.com/logout")
             # Wait for donwload to complete
             driver.implicitly_wait(1)
             paths = WebDriverWait(driver, 120, 1).until(download_wait)
             driver.implicitly_wait(2)
             # Quit browser
             driver.quit()
-            # Unzip download
+            # Unzip download and remove zip
             backup = ZipFile(data["document"] + ".zip")
             backup.extractall()
-            # Commit changes
-            version = data["version"]
-            new_version = version + 0.1
+            backup.close()
+            os.remove(data["document"] + ".zip")
+            # Create commit message
+            version = int(data["version"])
+            new_version = version + 1
             data["version"] = new_version
             with open("config.json", "w") as config:
-                json.dump(data, config)
-            msg = "V" + version + " -> V" + new_version
+                json.dump(data, config, sort_keys=True, indent=4, separators=(',', ': '))
+            msg = "V" + str(version) + " -> V" + str(new_version)
             if len(sys.argv) >= 3:
                 msg += ": " + str(sys.argv[2])
+            # Add, commit and push
             subprocess.call(["git", "add", "-A"])
-            subprocess.call(["git", "commit", "-m", str(sys.argv[2])])
+            subprocess.call(["git", "commit", "-m", msg])
+            subprocess.call(["git", "push", "-u", "origin", "master"])
         else:
-            print("Unknown command!")
+            print("Unknown command - Viable commands are:")
+            print("\tobs init [remote-repository-url]")
+            print("\tobs do [commit-message]")
+    else:
+        print("No command given - Viable commands are:")
+        print("\tobs init [remote-repository-url]")
+        print("\tobs do [commit-message]")
